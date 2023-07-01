8d28c18d260cff2ae1aa775152912b104f94aeff
Mohamed Abdelhady <73122852+mohamedabdelhady933@users.noreply.github.com>
2023-07-01 15:38:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,22 +8,18 @@ cd tools
 	chmod +x hoOk/hoOk.py
 
 
-# Git Dorks
-  git clone https://github.com/damit5/gitdorks_go.git
-  cd gitdorks_go
-  bash build.sh
-  cd ..
-  
-
 
   # joomla
-  git clone https://github.com/OWASP/joomscan
+  git clone https://github.com/OWASP/joomscan.git
+  
 
   #droopscan
   git clone https://github.com/droope/droopescan.git
   pip install -r droopescan/requirements.txt
+  
   # aem
-  git clone https://github.com/0ang3el/aem-hacker/
+  git clone https://github.com/0ang3el/aem-hacker.git
+  pip install -r aem-hacker/requirements.txt
   
 
 git clone https://github.com/IvanGlinkin/Fast-Google-Dorks-Scan.git
@@ -37,64 +33,184 @@ git clone https://github.com/IvanGlinkin/Fast-Google-Dorks-Scan.git
 	# chmod +x wpconfig-checker.py
 
 	# linkfinder
-	git clone https://github.com/GerbenJavado/LinkFinder.git
+	# git clone https://github.com/GerbenJavado/LinkFinder.git
+	# cd LinkFinder
+	# pip3 install -r requirements.txt
+	# python setup.py install
+	# cd ..
 
-	cd LinkFinder
-		pip3 install -r requirements.txt
-		python setup.py install
-	cd ..
+	# git clone https://github.com/m4ll0k/SecretFinder.git 
+	# python -m pip install -r /SecretFinder/requirements.txt
 
-	git clone https://github.com/m4ll0k/SecretFinder.git 
-	python -m pip install -r /SecretFinder/requirements.txt
 
-cd ..
 # subfinder
-go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+if  [ -x "$(command -v subfinder)" ]
+then
+
+else 
+	go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+ 	cp /root/go/bin/subfinder /user/local/bin/
+fi
 
 # sublist3r
-pip3 install sublist3r
+
+if  [ -x "$(command -v sublist3r)" ]
+then
+
+else 
+	pip3 install sublist3r
+fi
 
 # assetfinder
-go install -v github.com/tomnomnom/assetfinder@latest
+
+if  [ -x "$(command -v assetfinder)" ]
+then
+
+else 
+	go install -v github.com/tomnomnom/assetfinder@latest
+	cp /root/go/bin/assetfinder /user/local/bin/
+fi
 
 # amass
-go install -v github.com/owasp-amass/amass/v3/...@master
+
+# if  [ -x "$(command -v amass)" ]
+# then
+
+# else 
+# 	go install -v github.com/owasp-amass/amass/v3/...@master
+# 	cp /root/go/bin/amass /user/local/bin/
+# fi
 
 # gobuster
-go install github.com/OJ/gobuster/v3@latest
+
+if  [ -x "$(command -v gobuster)" ]
+then
+
+else 
+	go install github.com/OJ/gobuster/v3@latest
+	cp /root/go/bin/gobuster /user/local/bin/
+fi
+
 
 # subzy
-go install -v github.com/LukaSikic/subzy@latest
+
+if  [ -x "$(command -v subzy)" ]
+then
+
+else 
+	go install -v github.com/LukaSikic/subzy@latest
+	cp /root/go/bin/subzy /user/local/bin/
+fi
 
 # gau
-go install github.com/lc/gau/v2/cmd/gau@latest
+
+if  [ -x "$(command -v gau)" ]
+then
+
+else 
+	go install github.com/lc/gau/v2/cmd/gau@latest
+	cp /root/go/bin/gau /user/local/bin/
+fi
 
 # waybackurls
-go install -v github.com/tomnomnom/waybackurls@latest
+
+if  [ -x "$(command -v waybackurls)" ]
+then
+
+else 
+	go install -v github.com/tomnomnom/waybackurls@latest
+	cp /root/go/bin/waybackurls /user/local/bin/
+fi
+
 
 # httpx
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+
+if  [ -x "$(command -v httpx)" ]
+then
+
+else 
+	go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+	cp /root/go/bin/httpx /user/local/bin/
+fi
+
 
 #confused
-go install -v github.com/visma-prodsec/confused@latest
+# if  [ -x "$(command -v confused)" ]
+# then
+
+# else 
+# 	go install -v github.com/visma-prodsec/confused@latest
+# 	cp /root/go/bin/confused /user/local/bin/
+# fi
+
 
 # nuclei
-go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+
+if  [ -x "$(command -v nuclei)" ]
+then
+
+else 
+	go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+	cp /root/go/bin/nuclei /user/local/bin/
+fi
 
 # gospider
-go install -v github.com/jaeles-project/gospider@latest
+
+if  [ -x "$(command -v gospider)" ]
+then
+
+else 
+	go install -v github.com/jaeles-project/gospider@latest
+	cp /root/go/bin/gospider /user/local/bin/
+fi
+
 #kxss
-go install -v github.com/Emoe/kxss@latest
+
+if  [ -x "$(command -v kxss)" ]
+then
+
+else 
+	go install -v github.com/Emoe/kxss@latest
+	cp /root/go/bin/kxss /user/local/bin/
+fi
+
 #gitdorks_go
-go install -v github.com/damit5/gitdorks_go@latest
+
+if  [ -x "$(command -v gitdorks_go)" ]
+then
+
+else 
+	go install -v github.com/damit5/gitdorks_go@latest
+	cp /root/go/bin/gitdorks_go /user/local/bin/
+fi
 
 #dirsearch
-pip3 install dirsearch
-pip3 install arjun
+
+if  [ -x "$(command -v dirsearch)" ]
+then
+
+else 
+	pip3 install dirsearch
+fi
+
+#arjun
+if  [ -x "$(command -v arjun)" ]
+then
+
+else 
+	pip3 install arjun
+fi
+
 
 # wpscan
-gem install wpscan
 
+if  [ -x "$(command -v wpscan)" ]
+then
+
+else 
+	gem install wpscan
+ 	apt install wpscan
+fi
 
 
 
