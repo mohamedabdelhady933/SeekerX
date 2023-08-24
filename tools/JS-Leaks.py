13e0768e4e0780b819f03fcefcba8e2bae2f3105
Aly Khaled <71292235+0x41ly@users.noreply.github.com>
2023-08-24 23:08:38 +0300

    Update JS-Leaks.py

--- tools/JS-Leaks.py
+++ tools/JS-Leaks.py
@@ -20,7 +20,8 @@ if not os.path.exists(os.path.join(args.output, "js_files")):
 
 # Define the regex pattern to check for
 pattern = re.compile(r"""
-    api\w*\s*[:=]\s*(?P<api_key>['"\w]+)                      # API key
+    (?:password|passwd|pass)\s*[:=]\s*(?P<password>['"\w]+)  # password
+    |api\w*\s*[:=]\s*(?P<api_key>['"\w]+)                      # API key
     |aws.*?(?P<aws_key_id>AKIA\w{16})                          # AWS access key ID
     |['"]?(?P<secret_key>[A-Za-z0-9]{32,})['"]?                # secret key
     |(?P<oauth_key>oauth[_-]?(?:token|key))\s*[:=]\s*(?P<oauth_token>['"\w]+)  # OAuth token/key
