eb79989d3e49d90205fa9965608cdd9ccfd6a760
Mohamed Abdelhady <73122852+mohamedabdelhady933@users.noreply.github.com>
2023-09-23 15:06:13 +0300

    Update SeekerX.sh

--- SeekerX.sh
+++ SeekerX.sh
@@ -456,7 +456,7 @@ fi
   if [ -x "$(command gitdorks_go)" ] && ! [ -f $outputdir/$projectname/$1/.progress/.git-dorks] && [ "$GITHUB_TOKENS" != ""]
   then
         echo -e "\n${GREEN}[+] Scan for Github Dorks ${NC}\n"
-	gitdorks_go -gd $SEEKERX_HOME/tools/gitdorks_go/Dorks/smalldorks.txt -nws 25 -target $1 -token "${GITHUB_TOKENS}" -ew 5 >> $outputdir/$projectname/$1/recon/Dorks/git_dorks.txt
+	gitdorks_go -gd $SEEKERX_HOME/tools/gitdorks_go/Dorks/smalldorks.txt -nws 25 -target $1 -token $GITHUB_TOKENS -ew 5 >> $outputdir/$projectname/$1/recon/Dorks/git_dorks.txt
    	touch $outputdir/$projectname/$1/.progress/.git-dorks
   fi
 
